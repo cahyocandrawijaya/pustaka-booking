b11654d15806f52c2d943f6a2fdce66baa44c930
cahyocandrawijaya <cahyocandrawijaya56@gmail.com>
2022-03-20 20:21:49 +0700

    menambahkan index.php

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Cahyo Candra Wijaya, 
+              saya tinggal di Kranji
+              olahraga yang saya sukai adalah
+              sepakbola";
+    }
+}
